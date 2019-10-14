d451fa1b16f47f0de1589abb5c0648e7e760dae5
miyamoen <akihito.biblioteca@gmail.com>
2019-10-14 09:40:17 +0900

    sphere with ray marching

--- src/Main.elm
+++ src/Main.elm
@@ -8,7 +8,7 @@ import Element exposing (..)
 import Element.Background as Background
 import Element.Events exposing (onClick)
 import Html exposing (Html)
-import Html.Attributes as HAttrs
+import Html.Attributes as HAttrs exposing (style)
 import Html.Events
 import Json.Decode as JD
 import Math.Matrix4 as Mat4 exposing (Mat4)
@@ -19,7 +19,7 @@ import WebGL exposing (Mesh, Shader)
 
 size : number
 size =
-    256
+    512
 
 
 main : Program () Model Msg
@@ -98,11 +98,12 @@ view model =
                         [ HAttrs.width size
                         , HAttrs.height size
                         , HAttrs.style "display" "block"
+                        , HAttrs.style "width" "300px"
                         , Html.Events.on "mousemove" <| mouseDecoder MoveMouse
                         ]
                         [ WebGL.entity
                             vertexShader
-                            testShader
+                            sphereShader
                             mesh
                             { time = model.currentTime
                             , mouse = model.mouse
@@ -204,9 +205,83 @@ testShader =
         uniform vec2  mouse;
         uniform vec2  resolution;
 
-        void main (void) {
+        void main(void) {
+            // fragment position
             vec2 p = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);
-            vec2 color = (vec2(1.0) + p.xy) * 0.5;
-            gl_FragColor = vec4(color, 0.0, 1.0);
+
+            // camera
+            vec3 cPos = vec3(0.0,  0.0,  3.0); // カメラの位置
+            vec3 cDir = vec3(0.0,  0.0, -1.0); // カメラの向き(視線)
+            vec3 cUp  = vec3(0.0,  1.0,  0.0); // カメラの上方向
+            vec3 cSide = cross(cDir, cUp);     // 外積を使って横方向を算出
+            float targetDepth = 0.2;           // フォーカスする深度
+
+            // ray
+            vec3 ray = normalize(cSide * p.x + cUp * p.y + cDir * targetDepth);
+
+            // color
+            gl_FragColor = vec4(ray.xy, -ray.z, 1.0);
+        }
+    |]
+
+
+sphereShader : Shader {} Uniforms {}
+sphereShader =
+    [glsl|
+        precision mediump float;
+
+        uniform float time;
+        uniform vec2  mouse;
+        uniform vec2  resolution;
+
+        const vec3 lightDir = vec3(-0.577, 0.577, 0.577);
+        const float sphereSize = 1.0; // 球の半径
+
+        float distanceFunc(vec3 p){
+            return length(p) - sphereSize;
+        }
+
+        vec3 getNormal(vec3 p){
+            float d = 0.0001;
+            return normalize(vec3(
+                distanceFunc(p + vec3(  d, 0.0, 0.0)) - distanceFunc(p + vec3( -d, 0.0, 0.0)),
+                distanceFunc(p + vec3(0.0,   d, 0.0)) - distanceFunc(p + vec3(0.0,  -d, 0.0)),
+                distanceFunc(p + vec3(0.0, 0.0,   d)) - distanceFunc(p + vec3(0.0, 0.0,  -d))
+            ));
+        }
+
+        void main(void) {
+            // fragment position
+            vec2 p = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);
+
+            // camera
+            vec3 cPos = vec3(0.0,  0.0,  3.0); // カメラの位置
+            vec3 cDir = vec3(0.0,  0.0, -1.0); // カメラの向き(視線)
+            vec3 cUp  = vec3(0.0,  1.0,  0.0); // カメラの上方向
+            vec3 cSide = cross(cDir, cUp);     // 外積を使って横方向を算出
+            float targetDepth = 1.0;           // フォーカスする深度
+
+            // ray
+            vec3 ray = normalize(cSide * p.x + cUp * p.y + cDir * targetDepth);
+
+            // marching loop
+            vec3  rPos = cPos;    // レイの先端位置
+            for (int i = 0; i < 16; i++) {
+                float distance = distanceFunc(rPos);
+                if (distance < 0.001) {
+                    vec3 normal = getNormal(rPos);
+                    float diff = clamp(dot(lightDir, normal), 0.1, 1.0);
+                    gl_FragColor = vec4(vec3(diff), 1.0);
+                    return;
+                }
+                rPos +=  ray * distance;
+            }
+
+            // hit check
+            if (distanceFunc(rPos) < 0.001) {
+                vec3 normal = getNormal(rPos);
+                float diff = clamp(dot(lightDir, normal), 0.1, 1.0);
+                gl_FragColor = vec4(vec3(diff), 1.0);
+            }
         }
     |]
