6fd485724aaee96c50d9bcba8489422284bfdd47
miyamoen <akihito.biblioteca@gmail.com>
2019-11-28 01:59:09 +0900

    回転

--- src/Main.elm
+++ src/Main.elm
@@ -238,12 +238,30 @@ fragmentShader =
         const float PI = 3.14159265;
         const float fov = 120.0 * 0.5 * PI / 180.0;
 
+        // rotate
+        vec3 rotate(vec3 p, float angle, vec3 axis){
+            vec3 a = normalize(axis);
+            float s = sin(angle);
+            float c = cos(angle);
+            float r = 1.0 - c;
+            mat3 m = mat3(
+                a.x * a.x * r + c,
+                a.y * a.x * r + a.z * s,
+                a.z * a.x * r - a.y * s,
+                a.x * a.y * r - a.z * s,
+                a.y * a.y * r + c,
+                a.z * a.y * r + a.x * s,
+                a.x * a.z * r + a.y * s,
+                a.y * a.z * r - a.x * s,
+                a.z * a.z * r + c
+            );
+            return m * p;
+        }
 
-        // torus distance function
-        float distFuncTorus(vec3 p){
-            vec2 t = vec2(0.75, 0.25);
-            vec2 r = vec2(length(p.xy) - t.x, p.z);
-            return length(r) - t.y;
+        // smoothing min
+        float smoothMin(float d1, float d2, float k){
+            float h = exp(-k * d1) + exp(-k * d2);
+            return -log(h) / k;
         }
 
         // floor distance function
@@ -251,19 +269,38 @@ fragmentShader =
             return dot(p, vec3(0.0, 1.0, 0.0)) + 1.0;
         }
 
+        // box distance function
+        float distFuncBox(vec3 p){
+            return length(max(abs(p) - vec3(2.0, 0.1, 0.5), 0.0)) - 0.1;
+        }
+
+        // torus distance function
+        float distFuncTorus(vec3 p, vec2 r){
+          vec2 d = vec2(length(p.xy) - r.x, p.z);
+          return length(d) - r.y;
+        }
+
+        // cylinder
+        float distFuncCylinder(vec3 p, vec2 r){
+            vec2 d = abs(vec2(length(p.xy), p.z)) - r;
+            return min(max(d.x, d.y), 0.0) + length(max(d, 0.0)) - 0.1;
+        }
+
         // distance function
-        float distanceFunc(vec3 p){
-            float d1 = distFuncTorus(p);
-            float d2 = distFuncFloor(p);
-            return min(d1, d2);
+        float distFunc(vec3 p){
+            vec3 q = rotate(p, radians(time * 10.0), vec3(1.0, 0.5, 0.0));
+            float d1 = distFuncTorus(q, vec2(1.5, 0.25));
+            float d2 = distFuncBox(q);
+            float d3 = distFuncCylinder(q, vec2(0.75, 0.25));
+            return smoothMin(smoothMin(d1, d2, 16.0), d3, 16.0);
         }
 
         vec3 getNormal(vec3 p){
             float d = 0.0001;
             return normalize(vec3(
-                distanceFunc(p + vec3(  d, 0.0, 0.0)) - distanceFunc(p + vec3( -d, 0.0, 0.0)),
-                distanceFunc(p + vec3(0.0,   d, 0.0)) - distanceFunc(p + vec3(0.0,  -d, 0.0)),
-                distanceFunc(p + vec3(0.0, 0.0,   d)) - distanceFunc(p + vec3(0.0, 0.0,  -d))
+                distFunc(p + vec3(  d, 0.0, 0.0)) - distFunc(p + vec3( -d, 0.0, 0.0)),
+                distFunc(p + vec3(0.0,   d, 0.0)) - distFunc(p + vec3(0.0,  -d, 0.0)),
+                distFunc(p + vec3(0.0, 0.0,   d)) - distFunc(p + vec3(0.0, 0.0,  -d))
             ));
         }
 
@@ -280,12 +317,12 @@ fragmentShader =
             // marching loop
             vec3  dPos = cPos;    // レイの先端位置
             for (int i = 0; i < 256; i++) {
-                float distance = distanceFunc(dPos);
+                float distance = distFunc(dPos);
                 dPos +=  ray * distance;
             }
 
             // hit check
-            if (distanceFunc(dPos) < 0.001) {
+            if (distFunc(dPos) < 0.001) {
                 vec3 normal = getNormal(dPos);
                 float diff = clamp(dot(lightDir, normal), 0.1, 1.0);
                 gl_FragColor = vec4(vec3(diff), 1.0);
